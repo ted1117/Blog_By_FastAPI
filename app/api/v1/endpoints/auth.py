893651d651ea71fea3c9d38d6a50a591a77fe501
ted111712 <ted111712@gmail.com>
2025-07-08 14:35:15 +0900

    Docs: 독스트링 추가

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
@@ -10,12 +11,17 @@ from app.schema.user import UserCreate
 
 router = APIRouter()
 
-@router.post("/login", response_model=Token)
+@router.post("/login", response_model=Token, summary="로그인")
 def login(
     db: Session = Depends(get_db),
-    form_data: OAuth2PasswordRequestForm = Depends()
-):
+    form_data: OAuth2PasswordRequestForm = Depends(),
+) -> dict[str, str]:
     user = get_user_by_email(db, form_data.username)
+    """
+    회원 로그인을 진행하여 액세스 토큰을 반환합니다.
+    
+    이메일이나 비밀번호가 틀린 경우, `400 BAD REQUEST` 에러를 반환합니다.
+    """
     if not user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password")
     
@@ -26,10 +32,17 @@ def login(
 
     return {"access_token": access_token, "token_type": "bearer"}
 
-@router.post("/signup", response_model=Token)
+@router.post("/signup", summary="회원가입")
 def signup(user: UserCreate, db: Session = Depends(get_db)):
+    """
+    회원가입을 진행합니다.
+    
+    이미 가입된 회원이면 `400 BAD REQUEST`를 반환합니다.
+    """
     existing_user = get_user_by_email(db, user.email)
     if existing_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered.")
     
     new_user = create_user(db, user)
+
+    return {"detail": "Signup Success"}
--- app/api/v1/endpoints/post.py
+++ app/api/v1/endpoints/post.py
@@ -1,34 +1,41 @@
-from typing import List
-
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.api.deps import get_current_user, get_db
 from app.crud.post import create_post, delete_post, get_post, get_posts, update_post
 from app.models import User
+from app.models.post import Post
 from app.schema.post import PostCreate, PostRead, PostUpdate
 
 router = APIRouter()
 
 
-@router.get("/", response_model=List[PostRead])
+@router.get("/", response_model=list[PostRead], summary="게시글 목록 조회")
 def read_posts(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    """게시글을 10개 단위로 페이지네이션하여 반환합니다."""
     return get_posts(db, skip, limit)
 
-@router.get("/{post_id}", response_model=PostRead)
-def read_post(post_id: int, db: Session = Depends(get_db)):
+@router.get("/{post_id}", response_model=PostRead, summary="특정 게시글 조회")
+def read_post(post_id: int, db: Session = Depends(get_db)) -> Post:
+    """게시글 ID로 특정 게시글을 조회합니다."""
     db_post = get_post(db, post_id)
     if not db_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     
     return db_post
 
-@router.post("/", response_model=PostRead, status_code=201)
+@router.post("/", response_model=PostRead, status_code=status.HTTP_201_CREATED, summary="새로운 게시글 생성")
 def create_new_post(post_in: PostCreate, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    """새로운 게시글을 등록합니다."""
     return create_post(db, user.id, post_in)
 
-@router.put("/{post_id}", response_model=PostRead)
+@router.put("/{post_id}", response_model=PostRead, summary="기존 게시글 수정")
 def update_existing_post(post_id: int, post_in: PostUpdate, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+    """
+    기존 게시글을 수정합니다.
+
+    게시글은 작성자나 관리자만 수정할 수 있으며, 권한이 없는 경우 `403 Fobidden` 오류를 반환합니다.
+    """
     db_post = get_post(db, post_id)
     if not db_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
@@ -37,10 +44,15 @@ def update_existing_post(post_id: int, post_in: PostUpdate, user: User = Depends
     
     return update_post(db=db, db_post=db_post, post_in=post_in)
 
-@router.delete("/{post_id}")
+@router.delete("/{post_id}", summary="기존 게시글 삭제")
 def delete_existing_post(post_id: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
-    db_post = get_post(db, post_id)
+    """
+    기존 게시글을 삭제합니다.
     
+    게시글은 작성자나 관리자만 삭제할 수 있으며, 권한이 없는 경우 `403 Forbidden` 에러를 반환합니다.
+    """
+    db_post = get_post(db, post_id)
+
     if not db_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
     
